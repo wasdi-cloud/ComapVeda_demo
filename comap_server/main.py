3e6a563ebb1867de84ae388917787e3940d15daf
leonevalentina <valentina.leone@wasdi.cloud>
2025-12-08 17:26:15 +0100

    try TiTiler with COG file

--- comap_server/main.py
+++ comap_server/main.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
+from titiler.core.factory import TilerFactory
+from starlette.middleware.cors import CORSMiddleware
+from rio_tiler.io import Reader
+import morecantile
 
 oApp = FastAPI()
+
+# add CORS middleware
+oApp.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # allowss all origins - need to be more restrictive in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Tiler factory instance
+oCog = TilerFactory()
+
+# Register all the COG endpoints automatically
+oApp.include_router(oCog.router, tags=["Cloud Optimized GeoTIFF"])
+
+s_WEB_MERCATOR_TMS = morecantile.tms.get("WebMercatorQuad")
 
 @oApp.get("/")
 async def root():
@@ -16,6 +37,18 @@ async def get_three_points():
         ]
     }
 
+@oApp.get("/read_geotiff")
+async def read_geotiff():
+    """Reads a GeoTIFF file and returns its bounds."""
+    with Reader("TCI.tif") as oScr:
+        oBBox = oScr.get_geographic_bounds("epsg:4326")
+        print(oBBox)
+        iZoom = 5
+        # find all the tiles that cover the bounding box
+        aoTiles = list(oScr.tms.tiles(oBBox[0], oBBox[1], oBBox[2], oBBox[3], iZoom))
+        for oTile in aoTiles:
+            print(oTile)
+    return {"bounds": oBBox, "tiles_count": len(aoTiles)}
 
 if __name__ == "__main__":
     """
